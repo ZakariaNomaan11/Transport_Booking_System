6d02054b5bf0deea31c42cf3ee57f9d2a4972199
Zakaria Nomaan <zakarianomaan11@gmail.com>
2025-12-03 21:15:52 +0600

    Login Page

--- Nomaan.c
+++ Nomaan.c
@@ -2,6 +2,28 @@
 #include <stdlib.h>
 
 int main(){
-    printf("Hello, World!\n");
+    printf("_________________________________\n");
+    printf("            Bookers              \n");
+    printf("_________________________________\n");
+    int choice=0;
+    printf("Your Choice:\n");
+    printf("1. User Login\n");
+    printf("2. Admin Login\n");
+    scanf("%d",&choice);
+
+    if(choice==1){
+        printf("User Login Selected\n");
+    }
+    else if(choice==2){
+        printf("Admin Login Selected\n");
+    }
+    else{
+        printf("Invalid Choice\n");
+    }
+    
+    
+    
+    
+    
     return 0;
 }
